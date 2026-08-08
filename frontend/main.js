8745e21e270dba87c5e1f837e6fd3009ce7a37fd
FabioSantos99 <fsantos93x@gmail.com>
2026-08-08 19:52:17 -0300

    html and css created

---
+++ frontend/main.js
@@ -0,0 +1,15 @@
+import './css/style.css';
+import './css/container.css';
+import './css/header.css';
+import './css/responsive.css';
+import './css/theme.css';
+import './css/form.css';
+import './css/table.css';
+import './css/toolbar.css';
+import './css/edit.css';
+import './css/utilities.css';
+import './css/rowButtons.css';
+
+import './inventoryFunctions/addProducts.js'
+
+
