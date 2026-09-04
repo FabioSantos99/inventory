73a6ce768fb2052572d39b7e9329f755d776c918
FabioSantos99 <fsantos93x@gmail.com>
2026-09-04 19:47:55 -0300

    implementing docker

--- backend/database.js
+++ backend/database.js
@@ -5,9 +5,9 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: process.env.RENDER
+  ? { rejectUnauthorized: false }
+  : false,
 });
 
 const initDB = async () => {
